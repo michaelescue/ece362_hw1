7aaa3bb9f9117eb0792f7215b13f3cde2418aa74
Michael Escue <mescue@pdx.edu>
2019-01-16 03:59:50 -0800

    working on necessary recursive-ness

--- hw1/inputhdlr.c
+++ hw1/inputhdlr.c
@@ -13,8 +13,26 @@ DUe: 01/17/19
 #include "circularbuffer.h"
 
 
-char *inputhdlr(int current_linenum){
-	
+int inputhdlr(int current_linenum, char *p_input){
 
+	if(strncmp(p_input, "!", 1) == 0)		// Checks if entered string is !x command.
+	{
+		p_input = p_input + 1;		// Advance the pointer in the string to the [1] index, where [0] is !.
+		int linenum = 0;		// Temp value for passing the READ value. 
+		linenum = atoi(p);		// String input to integer value conversion.
+		linenum = abs(linenum);		// Absolute value.
+		p_input = p_input - 1;		// Reset pointer to beginning.
+		if(linenum == 0)		//Checks for error. There should never be a line "0". Also, abs() retuns 0 on failures.
+		{
+			printf("\'%s\' is not a valid command. Format is: ![integer]\n", p_input); //Error message
+			return 1;		// Return "unsuccessfull".
+		}
+		if((p_input = circularbuffer(READ, linenum, " ")) != NULL)		// Pass operation to READ, with linenum to search for, along with arbitrary string input.
+		{
+			circularbuffer(STORE, current_linenum, p_input);		//Store instruction.
+			inputhdlr(
+		}
 	}
+
+}
 	
